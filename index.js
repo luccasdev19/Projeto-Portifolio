d28f9ae70e8e92658c251c807b53cd1f07c59f7b
luccasdev19 <lucasrodrigues.oliveira0809@gmail.com>
2025-05-24 23:00:21 -0300

    adicionando o js

---
+++ index.js
@@ -0,0 +1,6 @@
+const prevbutton = document.getElementById('prev'); 
+const nextbutton = document.getElementById('next'); 
+const contentitem = document.querySelectorAll('.item');
+const contentdot = document.querySelectorAll('.dot'); 
+const contentnumbers = document.querySelector('.numbers'); 
+const contentlist = document.querySelector ('.list'); 
