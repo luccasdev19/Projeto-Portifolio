6153b33cb296284d1ce3d407b3d6460d2ea01710
luccasdev19 <lucasrodrigues.oliveira0809@gmail.com>
2025-05-26 23:05:07 -0300

    adicionando atualizações

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const contentitem = document.querySelectorAll('.item');
 const contentdot = document.querySelectorAll('.dot'); 
 const contentnumbers = document.querySelector('.numbers'); 
 const contentlist = document.querySelector ('.list'); 
+
+
+let active = 0; 
+const total = item.length; 
+let time; 
+
+function update(derection) {
+    document.querySelector ('.item.active').classList.remove(active) 
+    document.querySelector ('.dot.active').classList.remove(active)
+}
+
+prevbutton.addEventListener ('click', () => {
+    update (-1)
+})
+
+nextbutton.addEventListener ('click', () => {
+    update (1)
+})
